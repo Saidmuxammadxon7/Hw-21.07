3f6919e549ebe1a1c000cd4a568964415d013a53
Saidmuxammadxon7 <nothingusethisis@gmail.com>
2025-07-21 10:15:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log(2010)
+console.log("Ismail Jonkahone")
+
+// alert('Привет Саидухаммадхон')
+// alert('Пока')
+
+var name = "Alex"; // старый способ
+let age = 25; // современный, изменяемы й
+const PI = 3.14; // константа, нельзя менять
+
+// camelCase - стандарт
+
+let userName = "Alex";
+let productPrice = 5000;
+
+// новая строка в console.log()
+// console.log("Первая строка \n\nВторая строка");
+
+let user = prompt("String typedagi malumot kiriting");
+// alert("Имя," + user);   
+
+let number = prompt("Number typedagi malumot kiriting ");
+// alert("Фамиля," + surname); 
+
+let old = prompt("boolean typedagi malumot kiriting");
+// alert("Возраст," + old);
+confirm("Hamma malumotlarni to'g'ri kiritganizga ishonchiz komilmi?")
+alert(`Siz kiritgan ${user} sozi string typega tegishli \n ` + `Siz kiritgan ${number} soni number type \n` + `Siz kiritgan ${old} boolean type da`)
